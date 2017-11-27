1a46b5e89b1c9086e12d5e2acef4982d9fad0519
ziyang.wen <wenziyangday@gmail.com>
2017-11-27 20:36:21 +0800

    swiper 引入

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="recommend">
     <h1>{{ msg }}</h1>
-    <h2><router-link to="/Detail">Essential Links</router-link></h2>
+    <h2>
+      <router-link to="/Detail">Essential Links</router-link>
+    </h2>
   </div>
 </template>
 <script>
+  import { swiper, swiperSlide } from 'vue-awesome-swiper';
+  
   export default {
     name: 'recommend',
     data() {
       return {
         msg: 'Welcome to Your Vue.js App'
       }
+    },
+    components: {
+      swiper,
+      swiperSlide
     }
   }
 </script>
