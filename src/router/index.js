8b75d0e7bcff14fdb1c532658d649f2b339ebb02
ziyang.wen <wenziyangday@gmail.com>
2017-11-27 14:32:20 +0800

    路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '../components/Index.vue';
+import Recommend from '../components/Recommend.vue';
+import MV from '../components/MV.vue';
+import Sorting from '../components/Sorting.vue';
+import FM from '../components/FM.vue';
+import Detail from '../components/Detail.vue';
 
 Vue.use(Router);
 
@@ -8,8 +13,39 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index,
+      children: [
+        {
+          path: '/',
+          name: 'Recommend',
+          component: Recommend
+        },
+        {
+          path: '/Recommend',
+          component: Recommend
+        },
+        {
+          path: '/MV',
+          name: 'MV',
+          component: MV
+        },
+        {
+          path: '/Sorting',
+          name: 'Sorting',
+          component: Sorting
+        },
+        {
+          path: '/FM',
+          name: 'FM',
+          component: FM
+        }
+      ]
+    },
+    {
+      path: '/Detail',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
