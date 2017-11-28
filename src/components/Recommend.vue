69ab6ff2e63fad9b99eb8b60c510f999617d5bc4
ziyang.wen <wenziyangday@gmail.com>
2017-11-28 15:52:57 +0800

    推荐列表的初始化

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="recommend">
-    <h1>{{ msg }}</h1>
-    <h2>
+    <!--<h2>
       <router-link to="/Detail">Essential Links</router-link>
-    </h2>
+    </h2>-->
+    <ul class="recommend-list clearfix">
+      <li v-for="item in indexData.subjects">
+        <img :src="item.images.small" width="100%" alt="">
+        <div><a :href="{name: 'Detail', params: {id: item.id}}">{{item.title}}</a></div>
+        <div><router-link :to="{name: 'Detail', params: {id: item.id}}">{{item.title}}</router-link></div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
   import { swiper, swiperSlide } from 'vue-awesome-swiper';
+  import { mapState, mapActions } from 'vuex';
   
   export default {
     name: 'recommend',
-    data() {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
+    computed: {
+      ...mapState(['indexData'])
+    },
+    created() {
+      this.$store.dispatch('getIndexData');
     },
     components: {
       swiper,
@@ -22,3 +30,18 @@
     }
   }
 </script>
+
+<style>
+  .recommend-list {
+  
+  }
+  .recommend-list li {
+    float: left;
+    width: 50%;
+  }
+
+  .recommend-list li div {
+    line-height: 30px;
+    text-align: center;
+  }
+</style>
