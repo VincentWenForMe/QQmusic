4ea522471732ca6b7e9cbaf01e863c282307775e
ziyang.wen <wenziyangday@gmail.com>
2017-11-28 15:52:11 +0800

    详情获取

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="detail">
-    detail
+    detail {{this.$route.params}} {{detailData}}
   </div>
 </template>
 
 <script>
   export default {
-    name: 'detail'
+    name: 'detail',
+    data() {
+      return {
+        detailData: {}
+      }
+    },
+    created() {
+      this.$axios.get('/apis/movie/subject/' + this.$route.params.id).then(
+        response => {
+          console.log(response.data);
+          this.detailData = response.data;
+        }
+      ).catch(err => {
+        console.log(err);
+      })
+    }
   }
 </script>
 
